5f9a49f278b3001f1d893771734486dcd0895adf
Timo Riechel <triechel@osnatec.net>
2024-09-22 20:53:12 +0200

    App Init

---
+++ dw.recording.demo/dw.recording.demo/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace dw.recording.demo;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ dw.recording.demo/dw.recording.demo/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace dw.recording.demo;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
